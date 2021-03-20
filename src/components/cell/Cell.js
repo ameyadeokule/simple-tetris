ff79ee43da01c476dabbf573a0c4223b834239f0
Ameya Deokule <deokuleameya@gmail.com>
2021-03-20 18:31:53 +0530

    the game is working

--- src/components/cell/Cell.js
+++ src/components/cell/Cell.js
@@ -2,10 +2,11 @@ import React from 'react'
 import { StyledCell } from "./StyledCell";
 import { TETROMINOS } from "../../helper/tetrominos";
 
-const Cell = ({type}) => {
-    return (
-        <StyledCell type={type} color={TETROMINOS[type].color}>cell</StyledCell>
-    )
-}
-
-export default Cell
+const Cell = ({ type }) => (
+    <StyledCell type={type} color={TETROMINOS[type].color}>
+      {console.log('rerender cell')}
+    </StyledCell>
+  );
+  
+  export default React.memo(Cell);
+  
